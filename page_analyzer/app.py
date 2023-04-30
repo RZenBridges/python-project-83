fea4ad60f25467d79f4ae81613f78df32b10e6f2
RZen <bartellbe@gmail.com>
2023-04-30 10:44:41 +0000

    Update app.py pyproject.toml poetry.lock index.html Add footer.html head.html header.html index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, flash, get_flashed_messages,\
+                  redirect, render_template, request,\
+                  session, url_for
+import validators
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 
-@app.route('/')
-def first():
-    return render_template('index.html')
+@app.get('/')
+def get_first():
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('index.html', messages=messages)
+
+
+@app.post('/')
+def add_url():
+    URL = request.form.get('url')
+    if not validators.url(URL):
+        flash('Некорректный URL', 'error')
+        return redirect(url_for('get_first'))
+    flash('Страница успешно добавлена', 'success')
+    return redirect(url_for('get_first'))
